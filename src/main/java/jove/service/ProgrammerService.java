63307e46b91bdf233ce0d009eab7140d45766b02
Yeganathan S <hilariousengineer@protonmail.com>
2021-12-10 00:57:36 +0530

    Add services for ProjectLead, Programmer, ProjectOrder

--- src/main/java/jove/service/ProgrammerService.java
+++ src/main/java/jove/service/ProgrammerService.java
@@ -1,2 +1,43 @@
-package jove.service;public class Programmer {
+package jove.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import jove.entity.Programmer;
+
+import jove.repository.ProgrammerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProgrammerService {
+
+    @Autowired
+    private ProgrammerRepository programmerRepository;
+
+    @Transactional
+    public List<Programmer> getAllProgrammer() {
+        return programmerRepository.findAll();
+    }
+
+    @Transactional
+    public Optional<Programmer> getProgrammer(int id) {
+        return programmerRepository.findById(id);
+    }
+
+    @Transactional
+    public void addProgrammer(Programmer programmer) {
+        programmerRepository.save(programmer);
+    }
+
+    @Transactional
+    public void updateProgrammer(Programmer programmer) {
+        programmerRepository.save(programmer);
+    }
+
+    @Transactional
+    public void deleteProgrammer(int id) {
+        programmerRepository.deleteById(id);
+    }
 }
--- src/main/java/jove/service/SoftwareService.java
+++ src/main/java/jove/service/SoftwareService.java
@@ -1,2 +1,43 @@
-package jove.service;public class SoftwareService {
+package jove.service;
+import java.util.List;
+import java.util.Optional;
+
+import jove.entity.Software;
+
+import jove.repository.SoftwareRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SoftwareService {
+
+    @Autowired
+    private SoftwareRepository softwareRepository;
+
+    @Transactional
+    public List<Software> getAllSoftware() {
+        return softwareRepository.findAll();
+    }
+
+    @Transactional
+    public Optional<Software> getSoftware(int id) {
+        return softwareRepository.findById(id);
+    }
+
+    @Transactional
+    public void addSoftware(Software software) {
+        softwareRepository.save(software);
+    }
+
+    @Transactional
+    public void updateSoftware(Software software) {
+        softwareRepository.save(software);
+    }
+
+    @Transactional
+    public void deleteSoftware(int id) {
+        softwareRepository.deleteById(id);
+    }
+
 }
