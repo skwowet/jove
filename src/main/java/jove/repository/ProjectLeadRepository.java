5ee58eb8d4bbedaa9ea8e1f02ab33b5e2273095d
Yeganathan S <hilariousengineer@protonmail.com>
2021-12-10 00:59:09 +0530

    Add repositories for Programmer, ProjectLead, Software and test with dummy data

--- src/main/java/jove/repository/ProjectLeadRepository.java
+++ src/main/java/jove/repository/ProjectLeadRepository.java
@@ -1,2 +1,10 @@
-package jove.repository;public class ProjectLeadRepository {
+package jove.repository;
+
+import jove.entity.ProjectLead;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectLeadRepository extends JpaRepository<ProjectLead, Integer> {
+
 }
--- src/main/java/jove/repository/SoftwareRepository.java
+++ src/main/java/jove/repository/SoftwareRepository.java
@@ -1,2 +1,10 @@
-package jove.repository;public class SoftwareRepository {
+package jove.repository;
+
+import jove.entity.Software;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SoftwareRepository extends JpaRepository<Software, Integer> {
+
 }
