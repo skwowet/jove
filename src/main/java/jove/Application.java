d88ac7c623961eb1ee7c242ce032750e69ac5181
Yeganathan S <hilariousengineer@protonmail.com>
2021-12-10 00:59:09 +0530

    Add repositories for Programmer, ProjectLead, Software and test with dummy data

--- src/main/java/jove/Application.java
+++ src/main/java/jove/Application.java
@@ -1,14 +1,11 @@
 package jove;
 
+import jove.entity.*;
+import jove.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import jove.entity.Course;
-import jove.service.CourseService;
-import jove.service.StudentService;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner
@@ -18,22 +15,47 @@ public class Application implements CommandLineRunner
 	}
 
 	@Autowired
-	private StudentService studentService;
+	private ClientService clientService;
 
 	@Autowired
-	private CourseService courseService;
+	private ProjectOrderService projectOrderService;
+
+	@Autowired
+	private SoftwareService softwareService;
+
+	@Autowired
+	private ProjectLeadService projectLeadService;
+
+	@Autowired
+	private ProgrammerService programmerService;
 
 	@Override
 	public void run(String... args) throws Exception 
 	{
 		// Student student = new Student("werewfwer");
 		// studentService.save(student);
-		Course course = new Course("Chumma oru course");
-		courseService.save(course);
-		// student.addCourse(course);
-		// studentService.save(student);
-		// course.addStudent(student);
-		// courseService.save(course);
+//
+//		Course course = new Course("Chumma oru course");
+//		courseService.save(course);
+
+		Client client = new Client("Thara", "123456789", "thara133", "password");
+		clientService.addClient(client);
+
+		ProjectOrder project = new ProjectOrder("SoftwareName", "General", "12-23-2302", "12-04-2020", client);
+		projectOrderService.addProject(project);
+
+//		Create a software project
+		Software software = new Software("Hello", "23-09-2003", "30-02-2005", project);
+		softwareService.addSoftware(software);
+
+		Programmer programmer = new Programmer("TharaDas", 5, "Java, Python", "Software dev", "12-23-2020", software);
+		programmerService.addProgrammer(programmer);
+
+		ProjectLead projectLead = new ProjectLead("Kishore", 9, "Java, SpringBoot", "Web dev", "12-03-2009", software);
+		projectLeadService.addProjectLead(projectLead);
+
+
+
 	}
 
 }
--- src/main/java/jove/repository/ProgrammerRepository.java
+++ src/main/java/jove/repository/ProgrammerRepository.java
@@ -1,10 +1,10 @@
 package jove.repository;
 
-import jove.entity.Software;
+import jove.entity.Programmer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProgrammerRepository extends JpaRepository<Software, Integer> {
+public interface ProgrammerRepository extends JpaRepository<Programmer, Integer> {
 
 }
